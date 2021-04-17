5e81af877ba2ff702294e450825e984bbc35f8b3
Nauman Munir <naumanmunir@gmail.com>
2021-04-17 20:30:27 +0500

    validationSchemaYup => netWorth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import React, { FC } from "react";
 import { Card, CardContent } from "@material-ui/core";
 import { Field, Form, Formik } from "formik";
 import { TextField, CheckboxWithLabel } from "formik-material-ui";
-import React from "react";
+import { object, mixed, number } from "yup";
 
 const initialValueFormik = {
   firstName: "",
@@ -11,12 +12,26 @@ const initialValueFormik = {
   description: "",
 };
 
-const App = () => {
+const validationSchemaYup = object().shape({
+  netWorth: mixed().when("millionaire", {
+    is: true,
+    then: number()
+      .required()
+      .min(1000000, "You need to have a net worth of atleast 1 million"),
+    otherwise: number().required(),
+  }),
+});
+
+const App: FC = () => {
   return (
     <Card>
       <CardContent>
-        <Formik initialValues={initialValueFormik} onSubmit={() => {}}>
-          <Form>
+        <Formik
+          validationSchema={validationSchemaYup}
+          initialValues={initialValueFormik}
+          onSubmit={() => {}}
+        >
+          <Form autoComplete="off">
             <Field name="firstName" component={TextField} label="First Name" />
             <Field name="lastName" component={TextField} label="Last Name" />
             <Field
